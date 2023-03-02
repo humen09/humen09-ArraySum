3311e8add098da43a241927d26e426a011e4e41e
humen09 <humen08@gmail.com>
2023-03-02 14:03:45 +0000

    Completed ArraySum Class

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,9 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+int total = 0;
+for(int i : arr)
+total += i;
+        return total;
     }
 }
